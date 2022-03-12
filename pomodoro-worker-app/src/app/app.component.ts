96dfc4d383cef2be271305565ef6bcdc8df9e423
Vladislav Kramarukha <vladislav.kramarukha@magenta-technology.com>
2022-03-12 12:30:29 +0400

    (init): Initial commit

---
+++ pomodoro-worker-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pw-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'pomodoro-worker-app';
+}
